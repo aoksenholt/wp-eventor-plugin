acb49150ed626ef3b5ce06f9b4d27710d5b61a3f
Anders B. ?ksenholt <aoksenholt@gmail.com>
2011-04-27 18:46:27 +0200

    initial test with curl. fetch activities for NSK

--- EventorPlugin.php
+++ EventorPlugin.php
@@ -1,38 +1,77 @@
 <?php
 /*
  Plugin Name: EventorPlugin
- Plugin URI: http://www.example.com/myplugin
- Description: This is a really great plugin that extends WordPress.
- Version: 1.0.0
+ Plugin URI: http://nydalen.idrett.no/eventorplugin
+ Description: Plugin for fetching data from Eventor
+ Version: 0.0.1
  Author: nsk
  Author URI: http://nydalen.idrett.no
  */
 
+  define('EVENTOR_API_KEY', "8ebc1e96796547518d68a8b37059e95e");
+  define('EVENTOR_API_BASE_URL', "https://eventor.orientering.no/api/");
+  define('EVENTOR_ORGANISATION_ID', 245); //
+  define('EVENTOR_ACTIVITY_CACHE_TTL', 60*5);
 
-/**
- * Capitalizes the title given by $title.
- */
-function capitalizeTitle($title) {
-	return strtoupper($title);
-}
+  # Caching
+  define(CACHE, dirname(__FILE__) . '/cache/');
 
-/**
- * Adds a custom field that prompts the user for their favorite
- * color.
- * @return void
- */
-function drawCustomField() {
-	echo '<p><label>Favorite Color:<br />';
-	echo '<input autocomplete="off" class="input" name="fav_color" ';
-	echo ' id="fav_color" size="25"';
-	echo ' value="' . $_POST['fav_color'] . '" type="text" tabindex="32" />';
-	echo '</label><br /></p>';
-}
+  function eventorApiCall($url)
+  {
+    // create curl resource
+    $ch = curl_init();
+    // set url
+    curl_setopt($ch, CURLOPT_URL, $url);
+    // return the transfer as a string
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-/* now add the filter */
-add_filter('the_title', 'capitalizeTitle');
+    // set header
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array("ApiKey: " . EVENTOR_API_KEY));
 
-/* now add the action */
-add_action('register_form', 'drawCustomField');
+    // $output contains the output string
+    $output = curl_exec($ch);
+
+    // close curl resource to free up system resources
+    curl_close($ch);
+
+//echo $output;
+
+    return $output;
+  }
+
+  function getActivities() {
+    $data = "";
+
+    $cache .= CACHE . "activity.cache";
+
+    if (!file_exists($cache) || (file_exists($cache) && filemtime($cache) < (time() - EVENTOR_ACTIVITY_CACHE_TTL))) {
+echo "from eventor<br/>";
+
+      $url = EVENTOR_API_BASE_URL . "activities?organisationId=" . EVENTOR_ORGANISATION_ID . "&from=2011-01-01&to=2011-12-31&includeRegistrations=false";
+      $xml = eventorApiCall($url);
+
+      $activities = array();
+
+      $doc = simplexml_load_string($xml);
+      $activityNodes = $doc->xpath("//Activity");
+
+      foreach ($activityNodes as $activityNode) {
+        $name = $activityNode->xpath("Name");
+        $url = $activityNode->xpath("@url");
+        $registrationDeadline = $activityNode->xpath("@registrationDeadline");
+
+        $data .= "<a href=\"" . $url[0] . "\">" . $name[0] . "<a/> (" . $registrationDeadline[0] . ")<br/>";
+      }
+
+      $cachefile = fopen($cache, 'wb');
+      fwrite($cachefile, $data);
+      fclose($cachefile);
+    } else {
+echo "from cache (ttl: " . EVENTOR_ACTIVITY_CACHE_TTL . ")<br/>";
+      $data = file_get_contents($cache);
+    }
+
+    print $data;
+  }
 
 ?>
