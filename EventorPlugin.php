278c19895bc2a9aa5b1ac7fd3314458896b42c57
rune rystad <rune.rystad@gmail.com>
2011-05-04 17:19:27 +0200

    Etablering av Widget

--- EventorPlugin.php
+++ EventorPlugin.php
@@ -16,6 +16,17 @@
   # Caching
   define(CACHE, dirname(__FILE__) . '/cache/');
 
+
+  add_action('widgets_init', 'add_widget');
+  		
+
+  function add_widget()
+{
+  
+    require_once 'EventorWidget.php';
+	register_widget('Eventor_Widget_ClubDeadlines');
+}  
+   
   function eventorApiCall($url)
   {
     // create curl resource
@@ -24,37 +35,46 @@
     curl_setopt($ch, CURLOPT_URL, $url);
     // return the transfer as a string
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	
 
     // set header
     curl_setopt($ch, CURLOPT_HTTPHEADER, array("ApiKey: " . EVENTOR_API_KEY));
-
+	
     // $output contains the output string
     $output = curl_exec($ch);
 
+	if (!$output)
+		echo curl_error($ch);
+	
     // close curl resource to free up system resources
     curl_close($ch);
 
-//echo $output;
+//echo $url;
 
     return $output;
   }
 
+  
+  
   function getActivities() {
     $data = "";
 	$fromDate=date("Y-m-d");
     $cache .= CACHE . "activity.cache";
 
     if (!file_exists($cache) || (file_exists($cache) && filemtime($cache) < (time() - EVENTOR_ACTIVITY_CACHE_TTL))) {
-echo "from eventor<br/>";
+//echo "from eventor<br/>";
 
       $url = EVENTOR_API_BASE_URL . "activities?organisationId=" . EVENTOR_ORGANISATION_ID . "&from=" . $fromDate . "&to=2011-12-31&includeRegistrations=false";
       $xml = eventorApiCall($url);
 
+	  //echo $xml;
+	  
       $activities = array();
 
       $doc = simplexml_load_string($xml);
-      $activityNodes = $doc->Activity;
-
+      $activityNodes = $doc->Activity;	 
+	  
       foreach ($doc->Activity as $activity) {
         $name = $activity->Name;
         $url = $activity['url'];
@@ -65,19 +85,19 @@ echo "from eventor<br/>";
 		$date = new DateTime($registrationDeadline);
 		$registrationDeadline = $date->format('j/n H:i');
 		
-        $data .= "<a href=\"" . $url . "\">" . $name . "<a/> (" . $numRegistrations . ") - " . $registrationDeadline . "<br/>";
+        $data .= "<a href=\"" . $url . "\">" . $name . "</a> (" . $numRegistrations . ") - " . $registrationDeadline . "<br/>";
       }
 
       $cachefile = fopen($cache, 'wb');
       fwrite($cachefile, $data);
       fclose($cachefile);
     } else {
-echo "from cache (ttl: " . EVENTOR_ACTIVITY_CACHE_TTL . ", oldness " . (time()-filemtime($cache)) . ")<br/>";
+//echo "from cache (ttl: " . EVENTOR_ACTIVITY_CACHE_TTL . ", oldness " . (time()-filemtime($cache)) . ")<br/>";
       $data = file_get_contents($cache);
     }
 
     print $data;
   }
 
-  getActivities();
+  //getActivities();
 ?>
