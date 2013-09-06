2bc7a806f61c7c693adcd20851b92ef4097ee1b0
Anders B. ?ksenholt <aoksenholt@gmail.com>
2013-09-06 17:29:49 +0200

    issue#3

--- Queries/EventsForOrgsInMonthQuery.php
+++ Queries/EventsForOrgsInMonthQuery.php
@@ -3,7 +3,7 @@ class EventsForOrgsInMonthQuery extends Query
 {
 	public function getSupportedParameters()
 	{
-		return array('orgids' => $this->getOrgId(), 'month' => '06');
+		return array('orgids' => $this->getOrgId(), 'day' => date('d'), 'month' => date('m'), year => date('Y'));
 	}
 	
 	protected function getQueryUrl()
@@ -11,10 +11,16 @@ class EventsForOrgsInMonthQuery extends Query
 		$values = $this->getParameterValues();
 		
 		$orgIds = $values['orgids'];
+		$day = $value['day'];
 		$month = $values['month'];
-		
-		$url = "events?organisationIds=$orgIds&fromDate=2011-$month-01&toDate=2011-$month-31";
+		$year = $values['year'];
 
+		if ($orgIds != "") {
+			$url = "events?organisationIds=$orgIds&fromDate=$year-$month-$day&toDate=$year-$month-31";
+		} else {
+			$url = "events?fromDate=$year-$month-$day&toDate=$year-$month-31";
+		}
+		
 		return $url;
 	}
 	
