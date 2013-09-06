9e6ba098704cc32ea2c93fca2c16c070d20e136e
Anders B. ?ksenholt <aoksenholt@gmail.com>
2013-09-06 19:03:55 +0200

    version in build

--- EventorPlugin.php
+++ EventorPlugin.php
@@ -3,7 +3,7 @@
  Plugin Name: EventorPlugin
  Plugin URI: http://nydalen.idrett.no/eventorplugin
  Description: Plugin for fetching data from Eventor
- Version: 1.2.0
+ Version: @version@
  Author: nsk
  Author URI: http://nydalen.idrett.no
  */
